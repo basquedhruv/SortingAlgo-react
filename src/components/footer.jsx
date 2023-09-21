d2ead5e13f11022523296a62bea9c9f10191b26d
basquedhruv <dhruvrai01@gmail.com>
2023-09-22 03:31:45 +0530

    working functionality for sorting for several sorting algorithms

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created by Dhruv Rai
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Algorithms from './navbar-components/algorithms';
+import Size from './navbar-components/size';
+import Speed from './navbar-components/speed';
+
+
+class Navbar extends React.Component {
+    state = {
+        Algorithms: [
+			{ value: 1, type: 'Bubble Sort' },
+			{ value: 2, type: 'Twist Sort' },
+			{ value: 3, type: 'Insertion Sort' },
+			{ value: 4, type: 'Merge Sort' },
+			{ value: 5, type: 'Quick Sort' },
+            { value: 6, type: 'Heap Sort' },
+            { value: 7, type: 'Selection Sort' },
+		],
+		lengths: [15, 30, 45, 60, 75, 90, 105, 120, 135, 150],
+		speeds: [0.50, 0.75, 1.00, 2.00, 4.00]
+    };
+
+    
+    handleClick = (e) => {
+        e.preventDefault();
+        this.props.response();
+    }
+
+    render() {
+        return (
+            <div className="navbar" id="navbar">
+                <button id="random" onClick = {() => this.props.newList(1)}>Random</button>
+                <Algorithms 
+                    onChange = {this.props.onChange}
+                    algorithms = {this.state.Algorithms}
+                />
+                <Size 
+                    onChange = {this.props.onChange}
+                    lengths = {this.state.lengths}
+                />
+                <Speed 
+                    onChange = {this.props.onChange}
+                    speeds = {this.state.speeds}
+                />
+                <button id="start" onClick = {() => this.props.start()}>Start</button>
+                <a 
+                    className="icon" 
+                    onClick = {(e) => this.handleClick(e)}
+                    href = "/">
+                    <i className="fa fa-bars"></i>
+                </a>
+            </div>
+        );
+    }
+}
+ 
+export default Navbar;
